5e3591a7135ed9a5a442bdc0aa319d439a5ec3e1
swopmichalak <piotr.michalak@softwareone.com>
2024-11-14 15:16:06 +0100

    varss

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 # os.environ['test_user'] = 'abc'
 
 USER = os.environ['TEST_USER']
+USER_VAR = os.environ['USER_VAR']
 
 a = 2
 b = 5
 print("The result of the script is", a+b, "and will be used for", USER)
+print("The result of the script is", a+b, "and will be used for", USER_VAR)
