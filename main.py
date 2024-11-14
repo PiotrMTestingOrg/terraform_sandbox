9d52d586d7b800c36d87ffc92095d8e583345281
swopmichalak <piotr.michalak@softwareone.com>
2024-11-14 15:12:36 +0100

    adding env

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+# os.environ['test_user'] = 'abc'
+
+USER = os.environ['TEST_USER']
+
 a = 2
 b = 5
-print("The result of the script is", a+b)
+print("The result of the script is", a+b, "and will be used for", USER)
